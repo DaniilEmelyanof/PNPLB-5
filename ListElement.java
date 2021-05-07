43c50742620af3356a16edaabdcac51c66429d54
DaniilEmelyanof <83271932+DaniilEmelyanof@users.noreply.github.com>
2021-05-07 11:43:22 +0300

    Update ListElement.java

--- ListElement.java
+++ ListElement.java
@@ -5,8 +5,8 @@ public class ListElement
     private final String value;
     private ListElement next;
 
-    public ListElement (String value) {this.value = value;}//конструктор
-    public String getValue(){return value;}//возвращает значение элемента списка
-    public ListElement getNext() {return next;}//возвращает следующий элемент списка
-    public void setNext (ListElement next){this.next = next;}//определяет следующий элемент списка
+    public ListElement (String value) {this.value = value;} 
+    public String getValue(){return value;} 
+    public ListElement getNext() {return next;} 
+    public void setNext (ListElement next){this.next = next;} 
 }
